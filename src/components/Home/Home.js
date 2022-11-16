b107ef91646bcc7944d5973a0ab5ae0eabdb3f99
musananalour <94973185+Minjiaaa@users.noreply.github.com>
2022-11-15 18:31:26 -0800

    initial change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
               
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Chencheng Yi</strong>
+                <strong className="main-name"> Minjia Tao</strong>
               </h1>
  
               <div style={{ padding: 50, textAlign: "left" }}>
